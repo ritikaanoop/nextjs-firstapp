80df704fd03f1c823af569dbd1a6fde84bd32f88
Ritika Anoop <163532614+ritikaanoop@users.noreply.github.com>
2024-10-23 18:04:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-pink-100">
       <div className="text-center -mt-20">
-        <h1 className="text-4xl font-bold text-pink-400">Ritika Nair</h1>
+        <h1 className="text-4xl font-bold text-pink-400">Ritika :)</h1>
         <button 
           onClick={handleButtonClick} 
           className="mt-6 bg-pink-400 text-white px-4 py-2 rounded">
